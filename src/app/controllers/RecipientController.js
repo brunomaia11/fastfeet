d837a8788337f2cad8d822997da5e35b6a77823b
brunomaia11 <brunomaia11@gmail.com>
2020-03-25 23:23:57 -0300

    Gestão de destinatario

---
+++ src/app/controllers/RecipientController.js
@@ -0,0 +1,63 @@
+import * as Yup from 'yup';
+import Recipient from '../models/Recipient';
+
+class RecipientController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      street: Yup.string().required(),
+      state: Yup.string().required(),
+      city: Yup.string().required(),
+      zip_code: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const recipientExists = await Recipient.findOne({
+      where: { name: req.body.name },
+    });
+
+    if (recipientExists) {
+      return res.status(400).json({ error: 'Recipient alread exists' });
+    }
+
+    const { id, name, city, state, zip_code } = await Recipient.create(
+      req.body
+    );
+
+    return res.json({
+      id,
+      name,
+      city,
+      state,
+      zip_code,
+    });
+  }
+
+  async update(req, res) {
+    const { name } = req.body;
+    const recipient = await Recipient.findByPk(req.recipientId);
+
+    if (name && name !== recipient.name) {
+      const recipientExists = await Recipient.findOne({ where: { name } });
+
+      if (recipientExists) {
+        return res.status(400).json({ error: 'Recipient alread exists' });
+      }
+    }
+
+    const { id, city, state, zip_code } = await recipient.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      city,
+      state,
+      zip_code,
+    });
+  }
+}
+
+export default new RecipientController();
--- src/route.js
+++ src/route.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
+import RecipientController from './app/controllers/RecipientController';
 import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
 
@@ -11,5 +12,7 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+routes.post('/recipients', RecipientController.store);
+routes.put('/recipients', RecipientController.update);
 
 export default routes;
